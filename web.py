c4e3c0d8c487f196e40c0c615c4e6b68a13b044f
Vaclav Slajs <vaclav@slajs.xyz>
2017-08-23 21:21:12 +0200

    Rename page with wallet details

--- web.py
+++ web.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, jsonify, request
-from wallet import WalletStorage
+from flask import Flask, render_template
 from api import WalletResource
 
 app = Flask(__name__)
@@ -8,15 +7,11 @@ WalletResource.add_url_rules(app, rule_prefix='/api/wallet/')
 
 
 @app.route("/<wallet_id>")
-def wallet_index(wallet_id):
-    return render_template('index.html', wallet_id=wallet_id)
+def wallet_details(wallet_id):
+    return render_template('details.html', wallet_id=wallet_id)
 
 
 @app.route("/")
 def wallet_new():
-    return "unknow wallet"
-    # wallet = WalletStorage.create(coins_addresses=['0x1C7C21822fC30939A362fF3900C0b550D507bb41'],
-    #                               fiat_currency='EUR',
-    #                               invested=3428)
+    return render_template('index.html')
 
-    # return f"<a href='/{wallet.wallet_id}'>new wallet created</a>"
